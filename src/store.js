bc49e9e3fa9ac60e183218090fb522eb6e98b58b
rmozeika <robertmozeika@gmail.com>
2019-09-23 13:05:08 -0400

    .

--- src/store.js
+++ src/store.js
@@ -1,27 +1,27 @@
-import { applyMiddleware, createStore } from 'redux'
-import createSagaMiddleware from 'redux-saga'
-import rootReducer, { initialState } from './reducers'
-import rootSaga from './saga'
+import { applyMiddleware, createStore } from "redux";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { initialState } from "./reducers";
+import rootSaga from "./saga";
 
 const bindMiddleware = middleware => {
-  if (process.env.NODE_ENV !== 'production') {
-    const { composeWithDevTools } = require('redux-devtools-extension')
-    return composeWithDevTools(applyMiddleware(...middleware))
+  if (process.env.NODE_ENV !== "production") {
+    const { composeWithDevTools } = require("redux-devtools-extension");
+    return composeWithDevTools(applyMiddleware(...middleware));
   }
-  return applyMiddleware(...middleware)
-}
+  return applyMiddleware(...middleware);
+};
 
 function configureStore (initialState = {}) {
-  const sagaMiddleware = createSagaMiddleware()
+  const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
     rootReducer,
     initialState,
     bindMiddleware([sagaMiddleware])
-  )
+  );
 
-  store.sagaTask = sagaMiddleware.run(rootSaga)
+  store.sagaTask = sagaMiddleware.run(rootSaga);
 
-  return store
+  return store;
 }
 
-export default configureStore
+export default configureStore;
